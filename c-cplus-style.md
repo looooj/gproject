6aaaea2c819c8d82a582e725bfe6885d6b12097b
lijin <2553895012@qq.com>
2014-02-13 06:26:07 +0800

    add c-cplus-style

---
+++ c-cplus-style.md
@@ -0,0 +1,29 @@
+#c c++ code style
+
+    
+    no tab
+    indent space 4 
+
+    int create_object(const char* name, int size_count)
+    {
+        if ( name == null )
+            return -1;
+        //  
+        //....
+        //
+        return 0;
+    }
+    
+    
+    
+    
+    class Object
+    {
+    public:
+        void Execute(int flag);
+        
+    private:
+        int mId; 
+    }
+    
+    
---
+++ cpp/testxx/testiconv/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=testiconv
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/testiconv
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=testiconv.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/testiconv.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=testiconv
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/testiconv
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=testiconv.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/testiconv.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
